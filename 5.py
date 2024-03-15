92713d35fc72040832bdc1ff2ef1a30650962d9b
C:\prof <zdanovich23052006@gmail.com>
2024-03-16 02:20:45 +0300

    Initial commit

---
+++ 5.py
@@ -0,0 +1,35 @@
+import csv
+
+
+# словарь символов для хэш
+alf = sorted(' ёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ')
+d = {alf[i]: i + 1 for i in range(len(alf))}
+# print(dict)
+
+
+# функция задаёт строке уникальный хэш
+# s - входная строка, возвращает уникальный хэш этой строки
+def hassh(s):
+    p = 71
+    m = 10 ** 9 + 9
+    hash_value = 0
+    i = 0
+    for char in s:
+        hash_value += d[char] * p ** i
+        i += 1
+    return int(hash_value % m)
+
+
+# считываем файл students.csv и создаём файл students_with_hash.csv
+with open('students.csv', encoding='utf-8') as file, \
+        open('students_with_hash.csv', 'w', encoding='utf-8', newline='') as new_file:
+    data = list(csv.reader(file, delimiter=','))
+    res = csv.writer(new_file, delimiter=',')
+
+    # заполняем файл строками из исходного, заменяя id на хэш
+    res.writerow(data[0])
+    for stroka in data[1:]:
+        fio = stroka[1]
+        h = hassh(fio)
+        stroka[0] = h
+        res.writerow(stroka)
